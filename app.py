c0d3570c55d200545359015dd3d081851b2bfb5c
Syed Mohammed Shadaab <96412255+syedmohammedshadaab@users.noreply.github.com>
2024-08-16 23:47:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With LLAMA3.1 Model")
+input_text=st.text_input("What question you have in mind?")
+
+
+## Ollama LLAMA3.1 model
+llm=Ollama(model= "llama3.1")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+    
